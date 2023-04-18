a465962d664702615041461b78a8e2965170cbaa
Toshimitsu Ota <toshimitsu.ota@griffithuni.edu.au>
2023-04-18 16:37:12 +1000

    Update design elements with accent color and loading animation

--- Checklist/View/LoadingView.swift
+++ Checklist/View/LoadingView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 /// View to display loading status
 struct LoadingView: View {
+    /// state value loading for animation
+    @State private var isLoading = false
     var body: some View {
-        Text("Loading...")
+        Circle()
+            .trim(from: 0, to: 0.7)
+            .stroke(Color.green, lineWidth: 5)
+            .frame(width: 100, height: 100)
+            .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+            .animation(Animation.default.repeatForever(autoreverses: false))
+            .onAppear() {
+                isLoading = true
+                        }
     }
 }
 
